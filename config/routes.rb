7ea1317c68bf66f8c941a221497616d87080561a
Paul <paul.ogawa7@gmail.com>
2014-08-05 22:00:45 -0700

    added about and index routing pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: "pages#index"
-  get "pages/about", as: :about
+  get "pages/about"
   resources :scarves
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
