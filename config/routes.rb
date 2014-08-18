4ec606854e63abdf5868c7d63a4f45c07f40bac4
Paul Ogawa <paul.ogawa7@gmail.com>
2014-08-18 15:45:59 -0700

    adding asset precompile

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   root to: "pages#index"
   get "pages/about"
   resources :scarves
-  devise_for :users
+  devise_for :users, :controllers => { :registrations =>
+'users/registrations' }
+
   resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
